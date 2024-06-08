e9536af7d24df80eac6c24db17cb110c8457ab24
rani-98 <nagaranianji1998@gmail.com>
2024-06-08 12:01:27 +0530

    add use navigate hook,and print the data in table temporarly

---
+++ src/components/add_courses.jsx
@@ -0,0 +1,46 @@
+import React from "react"
+import ReactDOM from 'react-dom/client'
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+
+export default function add_courses() {
+    const [data,setData] = useState({});
+
+    const {course_name,course_price,course_duration} = data;
+    const navigate=useNavigate()
+
+    const changeHandler = e =>{
+        setData({...data,[e.target.name] : e.target.value})
+    }
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log(data); // Log data before navigating
+        navigate('/courses', { state: [data]  }); // Pass data in the state object
+    };
+    
+
+
+    return(
+        <div className="p-5 bg-gray-300">
+        <h1 className="text-2xl font-black p-7">Create New course details</h1>
+        <center>
+            <form onSubmit={submitHandler} className="flex flex-col gap-4 m-6 text-lg font-medium"> 
+                 
+                    <able>course name : 
+                    <input type="text" name="course_name" value={course_name} onChange={changeHandler}/></able>
+                    
+                    <label>course price : 
+                    <input type="text" name="course_price" value={course_price} onChange={changeHandler}/></label>
+                
+                    <label>course duration :
+                    <input type="number" name="course_duration" value={course_duration} onChange={changeHandler}/></label>
+                    
+                    <input className="w-20 h-10 rounded-md bg-green-400" type="submit" name="submit"></input>                    
+            </form>
+        </center>  
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(add_courses)
--- src/components/courses.jsx
+++ src/components/courses.jsx
@@ -1,40 +1,44 @@
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
-import { Link } from "react-router-dom"
-export default function courses(){
-    return(
-        <div className="p-7">
-        <div className="flex justify-content-space-between">
+export default function courses() {
+  const [table_Data,setTable_Data]=useState([])
+  const location = useLocation();
+    const data = location.state;
+    useEffect(() => {
+      // Check if data and data.data exist and if data.data is an array
+      if (data && Array.isArray(data)) {
+        setTable_Data(data);
+      }
+    }, [data]);
 
-        <h1 className="text-2xl font-black p-7"> All Courses</h1>
 
-        <button className="bg-green-400 rounded-md m-3 p-1 text-lg font-medium"><Link to="/add_courses">
-        + Add new course</Link></button>
-        </div>
+  return (
+    <>
+      <h1 className="text-2xl font-black p-7"> All Courses</h1>
+
+      <button className="flex justify-start bg-green-400 rounded-md m-3 p-1 text-lg font-medium">
+        <Link to="/add_courses">+ Add new course</Link>
+      </button>
+
+      <table className="w-full h-full gap-1 p-7 border-4 border-gray">
+        <tr className=" divide-x divide-y pt-4 bg-gray-400">
+          <th>course_name</th>
+          <th>course_duration</th>
+          <th>course_price</th>
+        </tr>
+        <tbody>
+          {table_Data.map((item,index)=>(
+          <tr key={index}>
+            <td>{item?.course_name || ''}</td>
+            <td>{item?.course_duration || ''}</td>
+            <td>{item?.course_price || ''}</td>
+          </tr>
+          ))}
+        </tbody>
+
         
-        
-        <table className="w-full h-full gap-1 p-7 border-4 border-gray">
-            <tr className=" divide-x divide-y pt-4 bg-gray-400">
-                <th >person name</th>
-                <th>course name</th>
-                <th>duration</th>
-                <th>course fee</th>
-            </tr>
-            <tr className="divide-x divide-y">
-                <td>rohit</td>
-                <td>python</td>
-                <td>6m</td>
-                <td>20000</td>
-            </tr>
-            <tr className=" divide-x divide-y ">
-                <td>geetha</td>
-                <td>java</td>
-                <td>6m</td>
-                <td>25000</td>
-            </tr>
-        </table>
-        
-        </div>
-    
-    
-    )
+      </table>
+    </>
+  )
 }
